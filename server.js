32477ce9ccda68f56b688d525537a2828a75251c
Boss <s.sittilerdpaisal@gmail.com>
2017-06-19 17:20:31 +0700

    Fixing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ function resolveURL(url) {
 var isUseHTTPs = false;
 
 // var port = 443;
-var port = 9001 || process.env.PORT || 9001;
+var port = process.env.PORT || 9001;
 
 var fs = require('fs');
 var path = require('path');
